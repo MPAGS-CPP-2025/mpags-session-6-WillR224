97e11e0659ee3c16e4d0aff13a19dd7b168bc069
WillR224 <will.rose.224@gmail.com>
2025-11-21 12:04:01 +0000

    Added the CMake file, and separated the functions into two separate cpp files along with the relevant header files

---
+++ src/CMakeFiles/mpags-cipher.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/workspaces/mpags-session-6-WillR224/src/MPAGSCipher/ProcessCommandLine.cpp" "CMakeFiles/mpags-cipher.dir/MPAGSCipher/ProcessCommandLine.cpp.o" "gcc" "CMakeFiles/mpags-cipher.dir/MPAGSCipher/ProcessCommandLine.cpp.o.d"
+  "/workspaces/mpags-session-6-WillR224/src/MPAGSCipher/TransformChar.cpp" "CMakeFiles/mpags-cipher.dir/MPAGSCipher/TransformChar.cpp.o" "gcc" "CMakeFiles/mpags-cipher.dir/MPAGSCipher/TransformChar.cpp.o.d"
+  "/workspaces/mpags-session-6-WillR224/src/mpags-cipher.cpp" "CMakeFiles/mpags-cipher.dir/mpags-cipher.cpp.o" "gcc" "CMakeFiles/mpags-cipher.dir/mpags-cipher.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/CMakeFiles/mpags-cipher.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mpags-cipher.dir/MPAGSCipher/ProcessCommandLine.cpp.o"
+  "CMakeFiles/mpags-cipher.dir/MPAGSCipher/ProcessCommandLine.cpp.o.d"
+  "CMakeFiles/mpags-cipher.dir/MPAGSCipher/TransformChar.cpp.o"
+  "CMakeFiles/mpags-cipher.dir/MPAGSCipher/TransformChar.cpp.o.d"
+  "CMakeFiles/mpags-cipher.dir/mpags-cipher.cpp.o"
+  "CMakeFiles/mpags-cipher.dir/mpags-cipher.cpp.o.d"
+  "mpags-cipher"
+  "mpags-cipher.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mpags-cipher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
